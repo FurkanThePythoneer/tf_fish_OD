c75ef04f1fc0d6d0c35424108692bc5711dcfd82
FurkanThePythoneer <73229980+FurkanThePythoneer@users.noreply.github.com>
2021-12-02 22:04:31 +0300

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -75,8 +75,7 @@ class FishDataset(Dataset):
             image = sample['image']
             
             if n_boxes > 0:
-                print('lllllll!!!')
-                print(sample['bboxes'])
+                print(len(sample['bboxes']))
                 target['boxes'] = torch.stack(tuple(map(torch.tensor, zip(*sample['bboxes'])))).permute(1, 0)
         else:
             image = ToTensorV2(p=1.0)(image=image)['image']
