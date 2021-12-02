ad7fcc43e1895e4784ffaecc7c09589f088d7ad4
FurkanThePythoneer <73229980+FurkanThePythoneer@users.noreply.github.com>
2021-12-02 21:44:44 +0300

    Initial commit

---
+++ dataset.py
@@ -0,0 +1,115 @@
+import numpy as np 
+import cv2
+import torch
+from torch.utils.data import Dataset
+import albumentations as albu
+from albumentations.pytorch import ToTensorV2
+from timm.data import IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD
+
+
+
+
+class FishDataset(Dataset):
+    def __init__(self, df, transforms=None):
+        self.df = df
+        self.transforms = transforms
+
+    def can_augment(self, boxes):
+        """ Check if bounding boxes are OK to augment
+        
+        
+        For example: image_id 1-490 has a bounding box that is partially outside of the image
+        It breaks albumentation
+        Here we check the margins are within the image to make sure the augmentation can be applied
+        """
+        
+        box_outside_image = ((boxes[:, 0] < 0).any() or (boxes[:, 1] < 0).any() 
+                             or (boxes[:, 2] > 1280).any() or (boxes[:, 3] > 720).any())
+        return not box_outside_image
+
+    def get_boxes(self, row):
+        """Returns the bboxes for a given row as a 3D matrix with format:
+         [x_min, y_min, x_max, y_max]"""
+        
+        boxes = pd.DataFrame(row['annotations'], columns=['x', 'y', 'width', 'height']).astype(float).values
+        
+        # Change from [x_min, y_min, w, h] to [x_min, y_min, x_max, y_max]
+        boxes[:, 2] = boxes[:, 0] + boxes[:, 2]
+        boxes[:, 3] = boxes[:, 1] + boxes[:, 3]
+        return boxes
+    
+    def get_image(self, row):
+        """Gets the image for a given row"""
+        
+        image = cv2.imread(f'{BASE_DIR}/{row["image_path"]}', cv2.IMREAD_COLOR)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB).astype(np.float32)
+        image /= 255.0
+        return image
+    
+    def __getitem__(self, i):
+
+        row = self.df.iloc[i]
+        image = self.get_image(row)
+        boxes = self.get_boxes(row)
+        
+        n_boxes = boxes.shape[0]
+        
+        # Calculate the area
+        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+            
+        target = {
+            'boxes': torch.as_tensor(boxes, dtype=torch.float32),
+            'area': torch.as_tensor(area, dtype=torch.float32),
+            'image_id': torch.tensor([i]),
+            'labels': torch.ones((n_boxes,), dtype=torch.int64),
+            'iscrowd': torch.zeros((n_boxes,), dtype=torch.int64)            
+        }
+
+        if self.transforms and self.can_augment(boxes):
+            sample = {
+                'image': image,
+                'bboxes': target['boxes'],
+                'labels': target['labels']
+            }
+            sample = self.transforms(**sample)
+            image = sample['image']
+            
+            if n_boxes > 0:
+                target['boxes'] = torch.stack(tuple(map(torch.tensor, zip(*sample['bboxes'])))).permute(1, 0)
+        else:
+            image = ToTensorV2(p=1.0)(image=image)['image']
+
+        return image, target
+
+    def __len__(self):
+        return len(self.df)
+
+
+def get_train_transform(image_sizes: list = [512,512]):
+    return albu.Compose([
+        albu.RandomResizedCrop(height=image_size[0], width=image_sizes[1], scale=(0.25, 1.0), ratio=(0.75, 1.3333333333333333), interpolation=1, p=1.0),
+        albu.ShiftScaleRotate(shift_limit=0.05, scale_limit=0.1, rotate_limit=30, interpolation=1, border_mode=0, value=0, p=0.25),
+        albu.HorizontalFlip(p=0.5),
+        albu.VerticalFlip(p=0.5),
+        albu.OneOf([
+            albu.MotionBlur(p=.2),
+            albu.MedianBlur(blur_limit=3, p=0.1),
+            albu.Blur(blur_limit=3, p=0.1),
+        ], p=0.25),
+        albu.OneOf([
+            albu.CLAHE(clip_limit=2),
+            albu.IAASharpen(),
+            albu.IAAEmboss(),
+            albu.RandomBrightnessContrast(),            
+        ], p=0.25),
+        #albu.Cutout(num_holes=8, max_h_size=32, max_w_size=32, fill_value=0, p=0.25),
+        #albu.Normalize(mean=IMAGENET_DEFAULT_MEAN, std=IMAGENET_DEFAULT_STD),
+        ToTensorV2(p=1.0)
+    ], bbox_params={'format': 'pascal_voc', 'label_fields': ['labels']})
+
+
+def get_valid_transform(image_sizes:list=[512, 512]):
+    return albu.Compose([
+        albu.Resize(image_sizes[0], image_sizes[1]),
+        ToTensorV2(p=1.0),
+        ], bbox_params={'format': 'pascal_voc', 'label_fields': ['labels']})
---
+++ train_model.py
@@ -0,0 +1,142 @@
+import cv2
+import time
+
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+import os
+import random
+import albumentations as albu
+from albumentations.pytorch.transforms import ToTensorV2
+
+import torch
+import torchvision
+from torch.utils.data import DataLoader, Dataset
+from models import MyModel
+from dataset import FishDataset, get_train_transform, get_valid_transform
+from madgrad import MADGRAD
+
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+NUM_EPOCHS = 20
+
+def seed_everything(seed=123):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = True
+
+def collate_fn(batch):
+   	return tuple(zip(*batch))
+
+def prepare_dataset():
+	df = pd.read_csv("/kaggle/input/reef-cv-strategy-subsequences-dataframes/train-validation-split/train-0.1.csv")
+
+	# Turn annotations from strings into lists of dictionaries
+	df['annotations'] = df['annotations'].apply(eval)
+
+	# Create the image path for the row
+	df['image_path'] = "video_" + df['video_id'].astype(str) + "/" + df['video_frame'].astype(str) + ".jpg"
+
+	df_train, df_val = df[df['is_train']], df[~df['is_train']]
+
+	df_train = df_train[df_train.annotations.str.len() > 0 ].reset_index(drop=True)
+	df_val = df_val[df_val.annotations.str.len() > 0 ].reset_index(drop=True)
+
+	ds_train = FishDataset(df_train, get_train_transform)
+	ds_valid = FishDataset(df_valid, get_valid_transform)
+
+
+
+	dl_train = DataLoader(ds_train, batch_size=8, shuffle=False, num_workers=4, collate_fn=collate_fn)
+	dl_val = DataLoader(ds_val, batch_size=8, shuffle=False, num_workers=4, collate_fn=collate_fn)
+
+
+	return dl_train, dl_val
+
+seed_everything(42)
+
+def get_model():
+    model = MyModel(
+    	backbone_name="resnet101d",
+    	imagenet_pretrained=False,
+    	num_classes=2,
+    	in_features=2048,
+    	backbone_pretrained_path=None, 
+    	backbone_pretrained_cls_num_classes=None,
+    	model_pretrained_path=None,
+    	model_pretrained_cls_num_classes=2)
+    return model.to(device)
+
+dl_train, dl_val = prepare_dataset()
+model = get_model()
+
+
+
+params = [p for p in model.parameters() if p.requires_grad]
+optimizer = MADGRAD(params, lr=3e-4)
+scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, NUM_EPOCHS-1)
+
+
+n_batches, n_batches_val = len(dl_train), len(dl_val)
+validation_losses = []
+
+
+for epoch in range(NUM_EPOCHS):
+    time_start = time.time()
+    loss_accum = 0
+    
+    for batch_idx, (images, targets) in enumerate(dl_train, 1):
+        
+        images = list(image.to(DEVICE) for image in images)
+        targets = [{k: v.to(DEVICE) for k, v in t.items()} for t in targets]
+
+        # Predict
+        loss_dict = model(images, targets)
+        losses = sum(loss for loss in loss_dict.values())
+        loss_value = losses.item()
+
+        loss_accum += loss_value
+
+        # Back-prop
+        optimizer.zero_grad()
+        losses.backward()
+        optimizer.step()
+
+    
+    # update the learning rate
+    if lr_scheduler is not None:
+        lr_scheduler.step()
+
+    # Validation 
+    val_loss_accum = 0
+        
+    with torch.no_grad():
+        for batch_idx, (images, targets) in enumerate(dl_val, 1):
+            images = list(image.to(DEVICE) for image in images)
+            targets = [{k: v.to(DEVICE) for k, v in t.items()} for t in targets]
+            
+            val_loss_dict = model(images, targets)
+            val_batch_loss = sum(loss for loss in val_loss_dict.values())
+            val_loss_accum += val_batch_loss.item()
+    
+    # Logging
+    val_loss = val_loss_accum / n_batches_val
+    train_loss = loss_accum / n_batches
+    validation_losses.append(val_loss)
+    
+    # Save model
+    chk_name = f'fasterrcnn_resnet101d-e{epoch}.bin'
+    torch.save(model.state_dict(), chk_name)
+    
+    
+    elapsed = time.time() - time_start
+    
+    print(f"[Epoch {epoch+1:2d} / {NUM_EPOCHS:2d}] Train loss: {train_loss:.3f}. Val loss: {val_loss:.3f} --> {chk_name}  [{elapsed:.0f} secs]")   
+
+
+
+
+
