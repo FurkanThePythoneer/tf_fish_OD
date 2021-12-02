0216a672e1bfd32e577a1a3e48ccade2a0e579e0
FurkanThePythoneer <73229980+FurkanThePythoneer@users.noreply.github.com>
2021-12-02 22:15:52 +0300

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -101,7 +101,7 @@ for epoch in range(NUM_EPOCHS):
 
         # Predict
         with torch.cuda.amp.autocast():
-        	loss_dict = model(images, targets)
+        	loss_dict = model(images.float(), targets.float())
         	losses = sum(loss for loss in loss_dict.values())
         	loss_value = losses.item()
 
